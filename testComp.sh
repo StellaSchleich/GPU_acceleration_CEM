31866339bf4319651c5fc1e784262a3734cc4d70
StellaSchleich <54136751+StellaSchleich@users.noreply.github.com>
2019-08-19 14:06:33 +0200

    Create testComp.sh

---
+++ testComp.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+#PBS -P MECH1240
+#PBS -q gpu_1
+#PBS -l select=1:ncpus=10:ngpus=1
+#PBS -l walltime=1:00:00
+#PBS -o /mnt/lustre/users/sschleich/example_2
+#PBS -e /mnt/lustre/users/sschleich/example_2
+#PBS -m abe
+#PBS -M 19909586@sun.ac.za
+
+
+ulimit -s unlimited   #to run in parallel with compiler
+source /etc/profile.d/modules.sh
+
+export LM_LICENSE_FILE=/apps/compilers/pgi/linux86-64/2019/license.dat
+module add chpc/pgi/19.4
+#module load chpc/pgi/19.4
+
+cd /mnt/lustre/users/sschleich/example_2
+
+#echo just prints
+echo -----------------------------------------------------------------------
+#run in current directory ./
+
+echo -------------------------------
+echo Complex
+gcc testComplex.c -o testComp
+./testComp
+
+
+echo -----------------------------------------------------------------------
